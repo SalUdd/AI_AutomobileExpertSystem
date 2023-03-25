d95ea6e1c6effce3c06a273155de89d03dd767f0
Salah Uddin <123456789+odin@users.noreply.github.com>
2023-03-26 02:52:42 +0800

    added more questions and answers in the rules for the expert system

--- test.py
+++ test.py
@@ -1,44 +1,117 @@
 import re
 
+#this is the rules that should be used by the expert system
 rules = {
-    '1': {
-        'question': '\nDoes the engine make any unusual noises?',
-        'options': {
-            'a': 'Yes',
-            'b': 'No'
-        },
-        'next': {
-            'a': '2',
-            'b': '3'
-        }
-    },
-    '2': {
-        'question': '\nWhat kind of noise is the engine making?',
-        'options': {
-            'a': 'Rattling',
-            'b': 'Knocking',
-            'c': 'Squeaking'
-        },
-        'next': {
-            'a': '4',
-            'b': '5',
-            'c': '6'
-        }
-    },
-    '3': {
-        'answer': '\n\033[1mPossible Problem\033[0m There may be no issues with the engine.\n\n'
-    },
-    '4': {
-        'answer': '\n\033[1mPossible Problem:\033[0m The engine may have a loose or damaged part.\n\n'
-    },
-    '5': {
-        'answer': '\n\033[1mPossible Problem:\033[0m The engine may have a bearing problem.\n\n'
-    },
-    '6': {
-        'answer': '\n\033[1mPossible Problem:\033[0m The engine may have a problem with the belts or pulleys.\n\n'
-    }
+  '1': {
+       'question': '\nDoes the engine start?',
+       'options': {
+           'a': 'Yes',
+           'b': 'No'
+       },
+       'next': {
+           'a': '2',
+           'b': '4'
+       }
+  },
+  '2': {
+       'question': '\nDoes the engine make any unusual noises?',
+       'options': {
+           'a': 'Yes',
+           'b': 'No'
+       },
+       'next': {
+           'a': '3',
+           'b': '8'
+       }
+   },
+   '3': {
+       'question': '\nWhat kind of noise is the engine making?',
+       'options': {
+           'a': 'Rattling',
+           'b': 'Knocking',
+           'c': 'Squeaking'
+       },
+       'next': {
+           'a': '14',
+           'b': '15',
+           'c': '16'
+       }
+   },
+   '4': {
+       'question': '\nDo you see the instrument panel or dashboard lights turn on?',
+       'options': {
+           'a': 'Yes',
+           'b': 'No'
+       },
+       'next': {
+           'a': '5',
+           'b': '13'
+       }
+   },
+   '5': {
+       'question': '\nPick one for the condition of the battery terminal:',
+       'options': {
+           'a': 'Corroded/ Dirty',
+           'b': 'Loose',
+           'c': 'Neither'
+       },
+       'next': {
+           'a': '11',
+           'b': '12',
+           'c': '6'
+       }
+   },
+   '6': {
+       'question': '\nDoes the electrical component like the dome light or other interior lights start bright and then go dim?',
+       'options': {
+           'a': 'Yes',
+           'b': 'No'
+       },
+       'next': {
+           'a': '10',
+           'b': '7'
+       }
+   },
+   '7': {
+       'question': '\nIs the car in park or neutral?',
+       'options': {
+           'a': 'Yes',
+           'b': 'No'
+       },
+       'next': {
+           'a': '8',
+           'b': '9'
+       }
+   },
+   '8': {
+       'answer': '\n\033[1mPossible Problem: No issue detected: \033[0m There may be no issues with the engine or other areas of the car. Take the car to the mechanic if the vehicle has issues.\n\n'
+   },
+   '9': {
+       'answer': '\n\033[1mPossible Problem: Put car in Park/ Neutral: \033[0mPut the car in park or neutral and start the car.\n\n'
+   },
+   '10': {
+       'answer': '\n\033[1mPossible Problem: Bad Alternator: \033[0mReplace alternator as soon as possible.\n\n'
+   },
+   '11': {
+       'answer': '\n\033[1mPossible Problem: Corroded or Dirty Battery Terminal: \033[0mTake the car to a mechanic to fix the corrosion/ dirt on battery terminal.\n\n'
+   },
+   '12': {
+       'answer': '\n\033[1mPossible Problem: Battery Terminal is loose: \033[0mTurn off the car. Then twist and turn the cables at the battery terminal.\n\n'
+   },
+   '13': {
+       'answer': '\n\033[1mPossible Problem: Dead Battery: \033[0mYou may have a weak battery and will need to charge or replace the battery.\n\n'
+   },
+   '14': {
+       'answer': '\n\033[1mPossible Problem: Loose or damaged engine part: \033[0m The engine may have a loose or damaged part. Visit a mechanic to fix the issue.\n\n'
+   },
+   '15': {
+       'answer': '\n\033[1mPossible Problem: Bearing Problem: \033[0m The engine may have a bearing problem. Replace bearings.\n\n'
+   },
+   '16': {
+       'answer': '\n\033[1mPossible Problem: Belts or Pulleys problem: \033[0m The engine may have a problem with the belts or pulleys. Replace belts or pulleys.\n\n'
+   }
 }
-
+#code for
 def ask_question(question, options):
     print(question)
     for option, value in options.items():
@@ -72,3 +145,5 @@ def run_expert_system(rules):
             current = '1'
 
 run_expert_system(rules)
+
+
